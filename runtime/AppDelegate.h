82f88bb70a51813247aac3fc4ad515eaaf365848
李雀 <lique@meituan.com>
2018-07-24 10:38:24 +0800

    Initial Commit

---
+++ runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime
+//
+//  Created by 李雀 on 2018/7/24.
+//  Copyright © 2018年 com.meituan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtime
+//
+//  Created by 李雀 on 2018/7/24.
+//  Copyright © 2018年 com.meituan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
